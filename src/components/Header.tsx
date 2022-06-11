61f4a67c292e5ecef68bf2ba487223af48e5d858
lex-nagorny <alex.nagorny.next@gmail.com>
2022-06-11 19:31:26 +0300

    Lesson_1-12 - CRA/ts - Create components with props: any

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Header = (props: any) => {
+    return <header className='header'>
+        <img
+            src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Ffree-vector%2Fancient-greek-coin-logo-design_57043-171.jpg%3Fsize%3D626%26ext%3Djpg&f=1&nofb=1'/>
+    </header>
+}
+
+export default Header;
